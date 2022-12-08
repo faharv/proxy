cd49de1a4490d99030f1ae1bac32302ca0c2768f
faharv <120080094+faharv@users.noreply.github.com>
2022-12-08 05:34:17 +0000

    Add files via upload

---
+++ stratum_mining.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe00cc06a4ctpvq39l89k -r stratum+tls://pool.whalesburg.com:4300 -p stratum;
+    sleep 5;
+done
