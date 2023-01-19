5838681b1a20222c2ed434e9c00c5a48c482ffe9
faharv <120080094+faharv@users.noreply.github.com>
2023-01-19 03:32:47 +0000

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while :; do
-    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe00cc06a4ctpvq39l89k -r community-pools.mysrv.cloud:10300 -p rpc;
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xe00cc06a4ctpvq39l89k -r dero.rabidmining.com:10300 -p rpc;
     sleep 5;
 done
